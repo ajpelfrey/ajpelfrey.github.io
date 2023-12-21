7aa19b42348ef1df327e57663651c5143e385b99
Alexander Pelfrey <58529497+ajpelfrey@users.noreply.github.com>
2023-12-20 20:06:52 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ajpelfrey.github.io",
+  base: "/ajpelfrey.github.io/",
   plugins: [react()],
 })
